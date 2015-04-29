17e357626b23f16266a26c8b7776ca7822406ccd
gonsalvj <gonsalvj@onid.oregonstate.edu>
2015-04-29 16:19:12 -0400

    cleaned up code format and remove html

--- assignment4/loopback.php
+++ assignment4/loopback.php
@@ -1,34 +1,9 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>CS 290 Assignment 4 - loopback</title>
-	</head>
-	<body>
-
-		<table>
-			<tbody>
-				<form action="loopback.php" method="get" enctype="multipart/form-data">
-				<tr><td>Enter min-multiplicand:</td><td><input name="min-multiplicand" type="text"></td></tr>
-				<tr><td>Enter max-multiplicand:</td><td><input name="max-multiplicand" type="text"></td></tr>
-				<tr><td>Enter min-multiplier:</td><td><input name="min-multiplier" type="text"></td></tr>
-				<tr><td>Enter max-multiplier:</td><td><input name="max-multiplier" type="text"></td></tr>
-				<tr><td> <input type="submit"  /></td></tr>
-				</form>
-			</tbody>
-		</table>
-	</body>
-
-</html>
-
 <?php 
 
 $request = array();
 $options = ["GET" => $_GET, "POST" => $_POST];
 $method = $_SERVER["REQUEST_METHOD"];
-
- $request['Type'] = $method;
- 
+$request['Type'] = $method; 
  
 foreach ($options[$method] as $key => $value) {
 	if(!isEmptyString($value)) {
@@ -38,11 +13,7 @@ foreach ($options[$method] as $key => $value) {
 if(count($request['parameters']) === 0) {
 	$request['parameters'] = null;
 }
-	
-
-
- echo json_encode($request);
-
+echo json_encode($request);
 
 function isEmptyString($string) {
 	if($string === '') {
@@ -50,5 +21,4 @@ function isEmptyString($string) {
 	}
 	return false; 
 }
-
 ?>
