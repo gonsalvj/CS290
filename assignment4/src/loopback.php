315f30cedd55b89425fae961b9d3714aa15dfd79
gonsalvj <gonsalvj@onid.oregonstate.edu>
2015-04-30 14:50:45 -0400

    tested and commented loopback.php code

--- assignment4/src/loopback.php
+++ assignment4/src/loopback.php
@@ -1,17 +1,20 @@
 <?php 
-
 $request = array();
 $options = ["GET" => $_GET, "POST" => $_POST];
+//get method
 $method = $_SERVER["REQUEST_METHOD"];
 $request['Type'] = $method; 
  
+ //get parameters from request and add to request array
 foreach ($options[$method] as $key => $value) {
 	if(!empty($value)) {
 		array_push($request['parameters'][$key] = $value);
 	}
 } 
+//if no parameters are present - set to null
 if(count($request['parameters']) === 0) {
 	$request['parameters'] = null;
 }
+//echo json object of request
 echo json_encode($request);
 ?>
